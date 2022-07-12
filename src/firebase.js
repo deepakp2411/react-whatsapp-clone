7099c45b631bdb1fd5a32529b77508849484b6e5
Deepak <bbaz2deepak@gmail.com>
2022-07-12 20:30:22 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0uoQN61bKKeDpzte2cCwat8ZSVxkL3jk",
+  authDomain: "whatsapp-clone-a7cd4.firebaseapp.com",
+  projectId: "whatsapp-clone-a7cd4",
+  storageBucket: "whatsapp-clone-a7cd4.appspot.com",
+  messagingSenderId: "114029884709",
+  appId: "1:114029884709:web:25dc94f66d726d9f8b56cc",
+  measurementId: "G-DG7EWG3THL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
